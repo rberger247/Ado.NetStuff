4c89890d0b88fa6ad5604977097c6720d148ae51
Rafael Berger <Rafiberger613@gmail.com>
2017-11-26 22:17:45 -0500

    initial

---
+++ Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class _Default : Page
+    {
+        private SqlConnection con;
+        private DataSet ds;
+        private SqlDataAdapter da;
+        private string cons = "Server=(localdb)\\mssqllocaldb;Database=FanAppDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            
+            ds = new DataSet();
+            DataTable dt = new DataTable();
+            con = new SqlConnection(cons);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Inbox";
+            con.Open();
+            da = new SqlDataAdapter(cmd);
+           
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            foreach (DataRow row in dt.Rows )
+            {
+                string field1 = row.ItemArray[0].ToString();
+
+            }
+        }
+    }
+}
---
+++ db.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using  System.Data;
+
+namespace WebApplication30
+{
+    public class db0
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+
+        public void addData()
+        {
+
+            SqlCommand com = new SqlCommand("select message from inbox", con);
+            con.Open();
+    
+            com.ExecuteNonQuery();
+            con.Open();
+                Console.ReadLine();
+            }
+        }
+
+    }
+ 
